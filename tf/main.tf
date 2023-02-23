55da8b010d19c84d969fb5581f296f4b80818414
AmarParamashetti <112462532+AmarParamashetti@users.noreply.github.com>
2023-02-23 20:51:54 +0530

    Create main.tf

---
+++ tf/main.tf
@@ -0,0 +1,17 @@
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-east1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/default-demo-app-e16ba/rqservice:ee8329a3349f7268611999ea873aa82d36d41076"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
