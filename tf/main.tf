c18a188c8b3a13b8d102f3da81327fc36b2468dd
AmarParamashetti <112462532+AmarParamashetti@users.noreply.github.com>
2023-02-23 19:25:19 +0530

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,24 +1,4 @@
 
-module "gh_oidc" {
-  source      = "github.com/terraform-google-modules/terraform-google-github-actions-runners.git//modules/gh-oidc?ref=v3.1.0"
-  project_id  = "default-demo-app-e16ba"
-  pool_id     = "my-pools"
-  provider_id = "my-providers"
-  attribute_mapping = {
-    "google.subject"             = "assertion.sub"
-    "attribute.actor"            = "assertion.actor"
-    "attribute.aud"              = "assertion.aud"
-    "attribute.repository_owner" = "assertion.repository_owner"
-    "attribute.sub"              = "assertion.sub"
-  }
-  sa_mapping = {
-    "my_service_account" = {
-      sa_name   = "projects/default-demo-app-e16ba/serviceAccounts/ghub-250@default-demo-app-e16ba.iam.gserviceaccount.com"
-      attribute = "attribute.repository/AmarParamashetti/gbactions"
-    }
-  }
-}
-
 resource "google_storage_bucket" "rq_backend_bucket" {
   name                        = "rqttfkbuck"
   location                    = "us-east1"
