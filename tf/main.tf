3c305cfc6eb9c01f29582a73d0c29ca355813145
AmarParamashetti <112462532+AmarParamashetti@users.noreply.github.com>
2023-02-23 23:12:55 +0530

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,9 @@
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+
+  disable_on_destroy = true
+}
+
 resource "google_cloud_run_service" "mydockerimage" {
   name     = "cloudrun-srv"
   location = "us-east1"
@@ -14,4 +20,12 @@ resource "google_cloud_run_service" "mydockerimage" {
     percent = 100
     latest_revision = true
   }
+   depends_on = [google_project_service.run_api]
+}
+
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+  service  = google_cloud_run_service.run_service.name
+  location = google_cloud_run_service.run_service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
